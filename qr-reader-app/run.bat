c21d1a1b3458d8aa4b5d6e8cf6bbab8d97faf12e
arturhc <arturh.sw@gmail.com>
2026-02-17 03:33:10 -0600

    ok

--- qr-reader-app/run.bat
+++ qr-reader-app/run.bat
@@ -4,6 +4,7 @@ setlocal
 set "SRC=src\main\java"
 set "OUT=out\classes"
 set "LIBS=libs\*"
+set "DEFAULT_ARGS=--fps 6 --threshold 0.10 --decode-window 4"
 
 echo === Compilando ===
 if not exist "%OUT%" mkdir "%OUT%"
@@ -12,7 +13,7 @@ javac -encoding UTF-8 -cp "%LIBS%" -d "%OUT%" "%SRC%\app\QrVideoReaderApp.java"
 if errorlevel 1 goto :error
 
 echo === Ejecutando lector ===
-java -cp "%OUT%;%LIBS%" app.QrVideoReaderApp %*
+java -cp "%OUT%;%LIBS%" app.QrVideoReaderApp %DEFAULT_ARGS% %*
 if errorlevel 1 goto :error
 
 goto :eof
